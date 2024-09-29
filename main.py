5de4e369717d2e888f885bf769eac59d1b4a4d93
Henrique Almeida <59674959+hicones@users.noreply.github.com>
2024-09-28 23:48:15 -0300

    feat: add websocket again

--- main.py
+++ main.py
@@ -6,21 +6,88 @@ from dotenv import load_dotenv
 from modules.standings import generate_standings_image, load_previous_standings, save_standings_json
 from modules.calendar import generate_matches_list
 from datetime import datetime, timedelta
+import websockets
+import json
 
 load_dotenv()
 
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
 FOOTBALL_API_TOKEN = os.getenv('FOOTBALL_API_TOKEN')
+WEBSOCKET_URL = os.getenv('WEBSOCKET_URL')
+WEBSOCKET_TOKEN = os.getenv('WEBSOCKET_TOKEN')
+
+WEBSOCKET_URL_WITH_TOKEN = f"{WEBSOCKET_URL}?token={WEBSOCKET_TOKEN}"
 
 intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='-corinthians ', intents=intents)
 
-
 @bot.event
 async def on_ready():
     print(f'{bot.user} está online!')
+
+def create_base_embed(author, record):
+    embed = discord.Embed(
+        title=author.get("displayName", "Usuário desconhecido"),
+        description=record.get("text", ""),
+        color=discord.Color.blue()
+    )
+    embed.set_author(
+        name=author.get("handle", "Usuário desconhecido"),
+        icon_url=author.get("avatar", "")
+    )
+    return embed
+
+def handle_images_in_embed(embed, images):
+    if images:
+        first_image = images[0].get("fullsize", "")
+        if first_image:
+            embed.set_image(url=first_image)
+        if len(images) > 1:
+            second_image = images[1].get("fullsize", "")
+            if second_image:
+                embed.set_thumbnail(url=second_image)
+    return embed
+
+async def send_embed_message(channel, post):
+    author = post.get('author', {})
+    record = post.get('record', {})
+    embed = create_base_embed(author, record)
+
+    if "embed" in post:
+        embed_data = post["embed"]
+        if embed_data["$type"] == "app.bsky.embed.images#view":
+            images = embed_data.get("images", [])
+            embed = handle_images_in_embed(embed, images)
+        elif embed_data["$type"] == "app.bsky.embed.external#view":
+            external = embed_data.get("external", {})
+            embed.set_author(name=author.get("displayName", "Autor desconhecido"))
+            embed.title = external.get("title", "Sem título")
+            embed.description = external.get("description", "Sem descrição")
+            embed.set_thumbnail(url=external.get("thumb", ""))
+            await send_external_embed(channel, embed, external)
+            return
+
+    await channel.send(embed=embed)
+
+async def send_external_embed(channel, embed, external):
+    class ExternalLinkView(discord.ui.View):
+        def __init__(self, uri):
+            super().__init__()
+            self.add_item(discord.ui.Button(label="Ver mais", url=uri, style=discord.ButtonStyle.link))
+
+    view = ExternalLinkView(uri=external.get("uri", "#"))
+    await channel.send(embed=embed, view=view)
+
+async def websocket_listener():
+    async with websockets.connect(WEBSOCKET_URL_WITH_TOKEN) as websocket:
+        while True:
+            message = await websocket.recv()
+            post = json.loads(message)
+            channel = bot.get_channel(CHANNEL_ID)
+            if channel:
+                await send_embed_message(channel, post)
 
 
 @bot.command(name="tabela")
